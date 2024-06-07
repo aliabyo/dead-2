fdbd1c2e1ac38b85cade5d700cbe1b0fa50c20da
aliabyo <161343889+aliabyo@users.noreply.github.com>
2024-06-06 21:42:44 -0700

    Delete src directory

--- src/events/join.py
+++
@@ -1,8 +0,0 @@
-from highrise.models import User
-from config.config import loggers
-
-
-async def on_join(bot, user: User) -> None:
-    if loggers.joins:
-        print(f"User joined: {user.username}:{user.id}")
-    await bot.highrise.chat(f"{user.username} Joined the room!")
--- src/events/react.py
+++
@@ -1,7 +0,0 @@
-from highrise.models import User, Reaction
-from config.config import loggers
-
-
-async def on_reaction(bot, user: User, reaction: Reaction, receiver: User) -> None:
-    if loggers.reactions:
-        print(f"{user.username} send {reaction} to {receiver.username}")
