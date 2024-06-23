1314fa6b6023cc96833c82232bb709f36af4904c
aliabyo <161343889+aliabyo@users.noreply.github.com>
2024-06-23 14:14:20 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,68 +1,53 @@
 from flask import Flask
 from threading import Thread
-from highrise import Position
-from highrise.__main__ import *
 from highrise.__main__ import *
 import time
-import traceback
-import time
-from highrise.models import (
-    Item,
-    Position,
-    Reaction,
-    SessionMetadata,
-    User,
-    AnchorPosition,
-    CurrencyItem ,
-)
+import os 
 
-class WebServer():
+class WebServer:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-  def __init__(self):
-    self.app = Flask(__name__)
+        @self.app.route('/')
+        def index() -> str:
+            return "Alive"
 
-    @self.app.route('/')
-    def index() -> str:
-      return "Alive"
+    def run(self) -> None:
+        self.app.run(host='0.0.0.0', port=5000)
 
-  def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8080)
-
-  def keep_alive(self):
-    t = Thread(target=self.run)
-    t.start()
+    def keep_alive(self):
+        t = Thread(target=self.run)
+        t.start()
 
 
-class RunBot():
-   
+class RunBot:
+    room_id = os.environ['Room_id']
+    bot_token = os.environ['Token']
+    bot_file = "main"
+    bot_class = "Bot"
 
-  room_id = "663018599dd1c89410d2f329"
-  bot_token = "e453d42a0bd9b0ea46da8c97e1e0dd61d5e5679206ebfec28067d9ab81ab3ad7"
-  bot_file = "main"
-  bot_class = "MyBot"
+    def __init__(self) -> None:
+        self.definitions = [
+            BotDefinition(
+                getattr(import_module(self.bot_file), self.bot_class)(),
+                self.room_id, self.bot_token)
+        ]  # More BotDefinition classes can be added to the definitions list
 
-  def __init__(self) -> None:
-    self.definitions = [
-        BotDefinition(
-            getattr(import_module(self.bot_file), self.bot_class)(),
-            self.room_id, self.bot_token)
-    ]  # More BotDefinition classes can be added to the definitions list
+    def run_loop(self) -> None:
+        while True:
+            try:
+                arun(main(self.definitions))
+            except Exception as e:
+                # Handle the exception
+                print("Caught an exception:")
+                print(e)
+                time.sleep(1)
+                continue
 
-  def run_loop(self) -> None:
-    while True:
-      try:
-        arun(main(self.definitions))
-        
-      except Exception as e:
-        # Print the full traceback for the exception
-        import traceback
-        print("Caught an exception:")
-        traceback.print_exc()  # This will print the full traceback
-        time.sleep(1)
-        continue
-    
 
 if __name__ == "__main__":
-  WebServer().keep_alive()
+    web_server = WebServer()
+    web_server.keep_alive()
 
-  RunBot().run_loop()
+    bot = RunBot()
+    bot.run_loop()
